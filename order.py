4e061b78e9287a656be402634cab049b19795abf
Gyanender Kumar <72467942+Gyani25k@users.noreply.github.com>
2024-07-02 00:47:48 +0530

    Order Superuser Code added

--- order.py
+++ order.py
@@ -3,11 +3,14 @@ from fastapi_jwt_auth import AuthJWT
 from fastapi.exceptions import HTTPException
 from models import User,Order
 from schemas import OrderModel
-from database import Session
+from database import engine,Session
+from fastapi.encoders import jsonable_encoder
+
 
 order_router = APIRouter(prefix='/order',
                          tags=['Orders'])
 
+session = Session(bind=engine)
 
 @order_router.get('/')
 async def hello(Authorize:AuthJWT=Depends()):
@@ -21,7 +24,7 @@ async def hello(Authorize:AuthJWT=Depends()):
     return {"message":"Hello World "}
 
 
-@order_router.post('/order')
+@order_router.post('/order',status_code=status.HTTP_201_CREATED)
 async def place_an_order(order:OrderModel,Authorize:AuthJWT=Depends()):
     try:
         Authorize.jwt_required()
@@ -34,3 +37,50 @@ async def place_an_order(order:OrderModel,Authorize:AuthJWT=Depends()):
     
     current_user = Authorize.get_jwt_subject()
 
+    user= session.query(User).filter(User.username==current_user).first()
+
+    new_order = Order(
+        pizza_size=order.pizza_size,
+        quantity = order.quantity
+    )
+
+    new_order.user = user
+
+    session.add(new_order)
+
+    session.commit()
+
+    response = {
+       "pizza_size":new_order.pizza_size,
+       "quantity":new_order.quantity,
+       "id":new_order.id,
+       "order_status":new_order.order_status 
+    }
+
+    return jsonable_encoder(response)
+
+
+@order_router.get('/order',status_code=status.HTTP_200_OK)
+async def get_all_orders(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Token"
+        )
+    
+    current_user = Authorize.get_jwt_subject()
+
+    user= session.query(User).filter(User.username==current_user).first()
+
+    if user.is_staff:
+        orders = session.query(Order).all()
+
+        return jsonable_encoder(orders)
+    
+    raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not a Superuser"
+    )
